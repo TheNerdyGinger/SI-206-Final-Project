dffd0fcbbfea3555db20c2e3fbee2bb07213d3c3
Stephen TNG <stephenfedewa@hotmail.com>
2019-12-10 13:00:20 -0500

    added failsafe in case category not found

--- off.py
+++ off.py
@@ -19,52 +19,52 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def set_up_ing_table(cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Ingredients")
-    cur.execute("CREATE TABLE Ingredients (ingredient_id INTEGER, ingredient TEXT)")
+# def set_up_ing_table(cur, conn):
+#     cur.execute("DROP TABLE IF EXISTS Ingredients")
+#     cur.execute("CREATE TABLE Ingredients (ingredient_id INTEGER, ingredient TEXT)")
 
-    query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
-    values = (0, "Flour")
-    cur.execute(query, values)
-    query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
-    values = (1, "Milk")
-    cur.execute(query, values)
-    query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
-    values = (2, "Butter")
-    cur.execute(query, values)
-    conn.commit()
+#     query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
+#     values = (0, "Flour")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
+#     values = (1, "Milk")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Ingredients (ingredient_id, ingredient) VALUES (?, ?)"
+#     values = (2, "Butter")
+#     cur.execute(query, values)
+#     conn.commit()
 
-def set_up_country_table(cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Countries")
-    cur.execute("DROP TABLE IF EXISTS Distances")
-    cur.execute("DROP TABLE IF EXISTS Lat_lng")
-    cur.execute("CREATE TABLE Countries (country_id INTEGER, country TEXT)")
-    cur.execute("CREATE TABLE Lat_lng (country_id INTEGER, latitude REAL, longitude REAL)")
-    cur.execute("CREATE TABLE Distances (country_id INTEGER, distance REAL)")
+# def set_up_country_table(cur, conn):
+#     cur.execute("DROP TABLE IF EXISTS Countries")
+#     cur.execute("DROP TABLE IF EXISTS Distances")
+#     cur.execute("DROP TABLE IF EXISTS Lat_lng")
+#     cur.execute("CREATE TABLE Countries (country_id INTEGER, country TEXT)")
+#     cur.execute("CREATE TABLE Lat_lng (country_id INTEGER, latitude REAL, longitude REAL)")
+#     cur.execute("CREATE TABLE Distances (country_id INTEGER, distance REAL)")
 
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (0, "France")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (1, "United States")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (2, "Spain")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (3, "Germany")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (4, "United Kingdom")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (5, "Brazil")
-    cur.execute(query, values)
-    query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
-    values = (6, "Portugal")
-    cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (0, "France")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (1, "United States")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (2, "Spain")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (3, "Germany")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (4, "United Kingdom")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (5, "Brazil")
+#     cur.execute(query, values)
+#     query = "INSERT INTO Countries (country_id, country) VALUES (?, ?)"
+#     values = (6, "Portugal")
+#     cur.execute(query, values)
  
-    conn.commit()
+#     conn.commit()
 
 def format_ingredient_name(ing):
     engine = inflect.engine()
@@ -133,7 +133,6 @@ def insert_into_tables(cur, conn, index, product_name, nutriscore, countries, in
     
     #Brands and scores insert
     query = "INSERT INTO Brands_and_scores (brand_id, brand, ingredient_id, nut_score) VALUES (?, ?, ?, ?)"
-  
     values = (bookmark, product_name, ingredient_id, nutriscore)
     cur.execute(query, values)
 
@@ -144,8 +143,33 @@ def insert_into_tables(cur, conn, index, product_name, nutriscore, countries, in
         cur.execute(query, values)
     conn.commit()
 
+def insert_if_not_found(cur, conn, ing_id):
+    cur.execute("SELECT ingredient FROM Ingredients WHERE ingredient_id = ?", (ing_id,))
+    name = cur.fetchone()[0]
 
+    #get last row
+    cur.execute("SELECT brand_id FROM Brands_and_scores WHERE brand_id = ?", (ing_id,))
+    repeat= cur.fetchone()
+    
+    if not repeat:
+        #get last row
+        cur.execute("SELECT * FROM Brands_and_scores ORDER BY brand_id DESC LIMIT 1")
+        bookmark = cur.fetchone()
+        if bookmark:
+            bookmark = bookmark[0]+1
+        else:
+            bookmark = 0
 
+        query = "INSERT INTO Brands_and_scores (brand_id, brand, ingredient_id, nut_score) VALUES (?, ?, ?, ?)"
+        values = (bookmark, name, ing_id, 0)
+        cur.execute(query, values)
+
+        #default to united states
+        query = "INSERT INTO Brand_countries_sold (brand_id, country_id) VALUES (?,?)"
+        values = (bookmark, 225)
+        cur.execute(query, values)
+        conn.commit()
+        
 def parse_json(data, cur, conn, ingredient_id):
     count = 0
     index = 0
@@ -217,13 +241,12 @@ def request_url(cur, conn):
         request_url = url.format(ing)
         r = requests.get(request_url)
         data = json.loads(r.text)
+        parse_json(data, cur, conn, pos) 
     except:
         print("Error reading from url")
-        # quit()
-        data = []
+        insert_if_not_found(cur, conn, pos)
+        return
     
-    parse_json(data, cur, conn, pos) 
-
     bookmark.close()
     pos +=1
     bookmark = open("off_bookmark.txt", 'w')
